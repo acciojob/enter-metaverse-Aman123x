cb8d5f92f4b9030aac253df522df1e4ce7650618
Aman Kumar <96618245+Aman123x@users.noreply.github.com>
2023-10-24 02:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const btn=document.getElementById("enterBtn");
+const pa=document.getElementById("status");
+
+btn.addEventListner("click",()=>{
+	let h1=document.createElement("h1");
+	h1.id="status";
+	h1.innerText="Entered Metaverse";
+	document.body.insertBefore(h1,pa);
+	pa.remove();
+})
